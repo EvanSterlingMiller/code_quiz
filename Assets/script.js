f57f1b2a52fbdd554aeaf1709955d56d5dfbc407
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-04-17 13:47:29 -0400

    more js

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+let question = [
+    {
+        questions:"Which element is used to insert the JavaScript into the HTML?",
+        selections: ["<Javascript>", "<js>", "<script>", "<scripting>" ],
+        answer: "<script>"
+    },
+    {
+        
+    }
+]
