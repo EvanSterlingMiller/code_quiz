1a407e032339f77775c5805083994c4e30397a08
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-04-17 18:28:29 -0400

    more js

---
+++ Assets/README.md
@@ -0,0 +1,27 @@
+## Introduction
+    
+    This is a quiz based on HTML, JavaScript, and CSS
+    
+## Table of Contents
+    
+    - [Installation](#installation)
+    - [Use](#use)
+    - [Contributers](#contributers)
+    - [License](#liscense)
+    
+## Installation
+    
+    There is no installation for this product. Everything is ran through the HTML page.
+    
+## Use
+    
+    This product is used to check your knowledge on coding.
+    
+## Contributers
+
+    Evan Sterling Miller
+
+## License
+
+    MIT
+    
--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,13 @@ let question = [
         answer: "<script>"
     },
     {
-        
-    }
+        questions:"What is the correct syntax refering to an external script called 'script.js?'?",
+        selections: ["<script src = 'script.js'>", "<script name = 'script.js'>", "<script href = 'script.js'>", "<script value = 'script.js>"],
+        answer: "<script src = 'script.js'>"
+    },
+    {
+        questions:"Which is the correct 'for' loop?",
+        selections:["for (i=0; i <=5)", "for (i=0; i<=5; i++)", "for i = 1 to 5",  "for (i<=5; i++"],
+        answer: "for (i=0; i<=5; i++)"
+    },
 ]
