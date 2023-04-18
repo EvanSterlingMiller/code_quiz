3151bf34a5da73d2cc477224a53eb0acf56d8415
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-04-17 20:38:33 -0400

    new HTML that is less confusing, more js

--- Assets/script.js
+++ Assets/script.js
@@ -14,4 +14,19 @@ let question = [
         selections:["for (i=0; i <=5)", "for (i=0; i<=5; i++)", "for i = 1 to 5",  "for (i<=5; i++"],
         answer: "for (i=0; i<=5; i++)"
     },
-]
+    {
+        questions:"Which is the orret way to add a comment in JavaScript?",
+        seletions:["//comment", "'commment", "<!--comment-->", "#comment"],
+        answer: "//comment"
+    },
+    {
+        questions:"How many times will this loop iterate 'for (x=1; x<11; x++)'",
+        selections:["9", "10", "11", "0"],
+        answer:"10"
+    },
+    {
+        questions:"What are the + - * and / symbols in JavaScript?",
+        selectors:["operators", "expressions", "functions", "objects"],
+        answer:"operators"
+    },
+]   
